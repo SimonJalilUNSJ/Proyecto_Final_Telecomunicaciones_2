30bcef0b62ae3640b33922309e10e74b5446bac1
Simón Jalil <simonjalilcruz@gmail.com>
2020-10-06 16:40:53 -0300

    Inicio del codigo bluetooth

---
+++ bluetooth/bluetooth.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial miBT(10,11);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Listo");
+  miBT.begin(38400);
+}
+
+void loop() {
+  if(miBT.available())   //Lee BT y envia a Arduino.
+    Serial.write(miBT.read());
+
+  if (Serial.available()) //Lee Arduino y envia a BT
+    miBT.write(Serial.read())
+   
+  
+}
