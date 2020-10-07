7b593325fa7a00243e0f089c70a43bd28c77f92b
Simón Jalil <simonjalilcruz@gmail.com>
2020-10-07 10:50:32 -0300

    Se agregan distintos programas de uso de lcd

---
+++ lcd_conteo/lcd_conteo.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7,6,5,4,3,2);
+
+void setup() {
+ lcd.begin(16,02);
+
+}
+
+void loop() {
+ lcd.setCursor(0,0);
+ lcd.print("Alarma activa");
+ lcd.display();
+ delay(500);
+ lcd.noDisplay();
+ delay(500);
+ 
+}
