16c518a94163d3e6d62e612208bbc8354d4d1a69
SimonJalilUNSJ <simonjalilcruz@gmail.com>
2020-10-07 12:32:02 -0300

    Modificacion del codigo encoder

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -1,9 +1,36 @@
+int dt = 2;
+int clk =  4;
+int anterior = 50;
+
+volatile int posicion = 50;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(dt,INPUT);
+  pinMode(clk,INPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(dt), encoder, LOW);
+  Serial.println("Listo");  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (posicion != anterior){
+    Serial.println(posicion);
+    anterior = posicion;
+  }
+}
 
+void encoder(){
+  static unsigned long ultimaInterrupcion = 0;
+  unsigned long tiempoInterrupcion = millis();
+
+  if (tiempoInterrupcion - ultimaInterrupcion > 5){
+    if (digitalRead(clk) == HIGH)
+      posicion++;
+    else 
+      posicion--;
+
+    posicion = min(100, max(0,posicion));  
+    ultimaInterrupcion = tiempoInterrupcion;
+  }
 }
